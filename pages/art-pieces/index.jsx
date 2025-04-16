fb6ed66596662fc631a0de88676c3965b15bf9a0
Angela Capillo <hello@angelcapillo.com>
2025-04-16 21:56:22 +0200

    Refactor ArtPiece and ArtPieceListComponents

--- pages/art-pieces/index.jsx
+++ pages/art-pieces/index.jsx
@@ -1,43 +1,13 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
+import ArtPiecesList from "@/components/ArtPieceList";
 
 const PageWrapper = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
 `;
-
-const ArtPieceList = styled.ul`
- display: flex;
- flex-direction: column;
- padding: 0;
- gap: 16px;
- max-width: 360px;
-`;
-
-const ArtPieceImage = styled(Image)`
- width: 300px;
- height: 200px;
- object-fit: contain;
-`;
-
-const ArtPieceWrapper = styled.li`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  list-style-type: none;
-  border-radius: 8px;
-  padding: 10px;
-  border: 1px solid #000;
-`;
-
-const ArtPieceLabel = styled.p`
-  max-width: 300px;
-  font-size: 0.825rem;
-  text-align: center;
-`;
-
 
 export default function Gallery() {
   const [artPieces, setArtPieces] = useState(""); // out big time state variable
@@ -59,16 +29,7 @@ export default function Gallery() {
   return (
     <PageWrapper>
       <h1>Gallery</h1>
-      <ArtPieceList>
-        {artPieces && // we check if artPieces exists (aka if it is true) and if yes we do a map loop through the array to show the art pieces
-          artPieces.map(({ slug, name, artist, imageSource }) => (
-            <ArtPieceWrapper key={slug}>
-              <ArtPieceImage src={imageSource} height={200} width={300}/>
-              <button></button>
-              <ArtPieceLabel>{`"${name}" by ${artist}`}</ArtPieceLabel>
-            </ArtPieceWrapper>
-          ))}
-      </ArtPieceList>
+      <ArtPiecesList artPieces={artPieces} />
     </PageWrapper>
   );
 }
