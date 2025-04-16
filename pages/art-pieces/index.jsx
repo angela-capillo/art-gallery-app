d99030ba62bb481cb3eadf29ed8b857554cace2a
Angela Capillo <hello@angelcapillo.com>
2025-04-16 20:52:21 +0200

    Refactor elements into styled components and add styling

--- pages/art-pieces/index.jsx
+++ pages/art-pieces/index.jsx
@@ -1,8 +1,45 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const PageWrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+const ArtPieceList = styled.ul`
+ display: flex;
+ flex-direction: column;
+ padding: 0;
+ gap: 16px;
+ max-width: 360px;
+`;
+
+const ArtPieceImage = styled(Image)`
+ width: 300px;
+ height: 200px;
+ object-fit: contain;
+`;
+
+const ArtPieceWrapper = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style-type: none;
+  border-radius: 8px;
+  padding: 10px;
+  border: 1px solid #000;
+`;
+
+const ArtPieceLabel = styled.p`
+  max-width: 300px;
+  font-size: 0.825rem;
+  text-align: center;
+`;
+
 
 export default function Gallery() {
-
   const [artPieces, setArtPieces] = useState(""); // out big time state variable
 
   useEffect(() => {
@@ -20,17 +57,18 @@ export default function Gallery() {
   //console.log("bla", artPieces);
 
   return (
-    <>
+    <PageWrapper>
       <h1>Gallery</h1>
-      <ul>
-        {artPieces && artPieces.map(({slug, name, artist, imageSource}) => (
-            <li key={slug}>
-            <Image src={imageSource} height={200} width={300}/>
-            <button></button>
-            <p>{name + " by " + artist}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+      <ArtPieceList>
+        {artPieces && // we check if artPieces exists (aka if it is true) and if yes we do a map loop through the array to show the art pieces
+          artPieces.map(({ slug, name, artist, imageSource }) => (
+            <ArtPieceWrapper key={slug}>
+              <ArtPieceImage src={imageSource} height={200} width={300}/>
+              <button></button>
+              <ArtPieceLabel>{`"${name}" by ${artist}`}</ArtPieceLabel>
+            </ArtPieceWrapper>
+          ))}
+      </ArtPieceList>
+    </PageWrapper>
   );
 }
