bf58f8f9ba8195e3c63931b046a5a771cb993634
Angela Capillo <hello@angelcapillo.com>
2025-04-17 14:50:59 +0200

    Create a separate component for Spotlight

--- components/SpotlightArtPiece.jsx
+++ components/SpotlightArtPiece.jsx
@@ -1,20 +1,32 @@
 import ArtPiece from "@/components/ArtPiece";
+import { useState, useEffect } from "react";
 
 export default function SpotlightArtPiece({ artPieces }) {
+  const [randomArtPiece, setRandomArtPiece] = useState(null);
+
+  console.log("outside use effect", randomArtPiece);
+
   function getRandomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
+
+  useEffect(() => {
+    setRandomArtPiece(getRandomElement(artPieces));
+
+    console.log("inside use effect", randomArtPiece);
+    
+  }, [artPieces])
 
   if (!artPieces || artPieces.length === 0) {
     return <p>No art pieces to show yet.</p>;
   }
 
-  let randomArtPiece = getRandomElement(artPieces);
-  console.log("random", randomArtPiece);
-  console.log("slug", randomArtPiece.slug);
+  //let randomArtPiece = getRandomElement(artPieces);
+  //console.log("random", randomArtPiece);
+  //console.log("slug", randomArtPiece.slug);
 
   return (
-    <ArtPiece
+    randomArtPiece &&<ArtPiece
       slug={randomArtPiece.slug}
       name={randomArtPiece.name}
       artist={randomArtPiece.artist}
